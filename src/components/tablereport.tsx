384455ee81e11248a683bea39222c435bfd12680
Dwiki Okvian Pradana <dwikiokvianpradana@gmail.com>
2025-03-06 14:30:57 +0700

    feat - add daftar riwayat pelanggaran

---
+++ src/components/tablereport.tsx
@@ -0,0 +1,119 @@
+'use client'
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+const invoices = [
+  {
+    no: "INV001",
+    nama: "Dwiki Okvian Pradana",
+    jabatan: "Supervisor",
+    jenisPelanggaran: "Pelanggaran Ringan",
+    lokasiPelanggaran: "Dekat kantor"
+  },
+  {
+    no: "INV001",
+    nama: "Dwiki Okvian Pradana",
+    jabatan: "Supervisor",
+    jenisPelanggaran: "Pelanggaran Ringan",
+    lokasiPelanggaran: "Dekat kantor"
+  },
+  {
+    no: "INV001",
+    nama: "Dwiki Okvian Pradana",
+    jabatan: "Supervisor",
+    jenisPelanggaran: "Pelanggaran Ringan",
+    lokasiPelanggaran: "Dekat kantor"
+  },
+  {
+    no: "INV001",
+    nama: "Dwiki Okvian Pradana",
+    jabatan: "Supervisor",
+    jenisPelanggaran: "Pelanggaran Ringan",
+    lokasiPelanggaran: "Dekat kantor"
+  },
+  {
+    no: "INV001",
+    nama: "Dwiki Okvian Pradana",
+    jabatan: "Supervisor",
+    jenisPelanggaran: "Pelanggaran Ringan",
+    lokasiPelanggaran: "Dekat kantor"
+  },
+  {
+    no: "INV001",
+    nama: "Dwiki Okvian Pradana",
+    jabatan: "Supervisor",
+    jenisPelanggaran: "Pelanggaran Ringan",
+    lokasiPelanggaran: "Dekat kantor"
+  },
+  {
+    no: "INV001",
+    nama: "Dwiki Okvian Pradana",
+    jabatan: "Supervisor",
+    jenisPelanggaran: "Pelanggaran Ringan",
+    lokasiPelanggaran: "Dekat kantor"
+  },
+  {
+    no: "INV001",
+    nama: "Dwiki Okvian Pradana",
+    jabatan: "Supervisor",
+    jenisPelanggaran: "Pelanggaran Ringan",
+    lokasiPelanggaran: "Dekat kantor"
+  },
+  {
+    no: "INV001",
+    nama: "Dwiki Okvian Pradana",
+    jabatan: "Supervisor",
+    jenisPelanggaran: "Pelanggaran Ringan",
+    lokasiPelanggaran: "Dekat kantor"
+  },
+  {
+    no: "INV001",
+    nama: "Dwiki Okvian Pradana",
+    jabatan: "Supervisor",
+    jenisPelanggaran: "Pelanggaran Ringan",
+    lokasiPelanggaran: "Dekat kantor"
+  },
+  {
+    no: "INV001",
+    nama: "Dwiki Okvian Pradana",
+    jabatan: "Supervisor",
+    jenisPelanggaran: "Pelanggaran Ringan",
+    lokasiPelanggaran: "Dekat kantor"
+  },
+]
+
+export function TableReports() {
+  return (
+    <Table>
+      <TableCaption>Daftar Riwayat Pelanggaran</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Nama</TableHead>
+          <TableHead>Jabatan</TableHead>
+          <TableHead>Jenis Pelanggaran</TableHead>
+          <TableHead className="text-right">Lokasi Pelanggaran</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {invoices.map((invoice, index) => (
+          <TableRow key={index}>
+            <TableCell className="font-medium">{invoice.nama}</TableCell>
+            <TableCell>{invoice.jabatan}</TableCell>
+            <TableCell>{invoice.jenisPelanggaran}</TableCell>
+            <TableCell className="text-right">{invoice.lokasiPelanggaran}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+      <TableFooter>
+      </TableFooter>
+    </Table>
+  )
+}
